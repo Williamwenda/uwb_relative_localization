2832f70a5b84e6c707edfc5b313a72304009171d
wenda <wenda.zhao@mail.utoronto.ca>
2021-12-07 19:47:31 -0500

    one walk around for checking the image

--- test.c
+++ test.c
@@ -28,8 +28,12 @@ PI_L2 unsigned char *buff;
 PI_L2 unsigned char *buff_demosaick;
 PI_L2 unsigned char value;
 
-#define UART_RCV_SIZE 28
-#define PACKET_SIZE 24
+// #define UART_RCV_SIZE 28
+// #define PACKET_SIZE 24
+
+#define UART_RCV_SIZE 12     // 2 x 4 (float bytes) + 4 = 12 
+#define PACKET_SIZE 8        // 2 x 4 (float bytes) = 8
+
 static PI_L2 unsigned char buff_uart_rcv[UART_RCV_SIZE];
 static PI_L2 unsigned char buff_packet[PACKET_SIZE];
 static PI_L2 unsigned char buff_packet_tmp[PACKET_SIZE];
@@ -42,22 +46,20 @@ void uart_rx_cb(void *arg);
 static struct pi_device camera;
 static struct pi_device flash;
 
-#define READSIZE 1600
-uint32_t flash_address = 0; // 300*(CROP_SIZE+PACKET_SIZE); // start address in Flash
+#define READSIZE 888             // 512 Mbit -> 64MB flash; 64000 kb / 72 kb(per img) = 888.88   
+uint32_t flash_address = 0;      // 300*(CROP_SIZE+PACKET_SIZE);     // start address in Flash
 char image_name[13];
-uint16_t image_number = 0;
 
+static uint16_t image_number = 0;    
 static float previousRoll;
-// float roll, pitch, pos_x, pos_y, pos_z, pos_d;
+float cf_tick, pos_z;
+static bool start_storing = true;
 
-// [change]
-// float cf_tick;
-float cf_tick, pos_z, acc_z, gyro_x, gyro_y, gyro_z;
-
-static bool start_storing = false;
-
-static float decode_packet(uint8_t *src, float * r, float * p, float * x, float * y, float * z, float * d);
+static float decode_packet(uint8_t *src, float * cf_tick, float * pos_z);
 static float buff_strip(uint8_t *src, uint8_t *tar);
+
+// check img end
+static bool IMG_END;
 
 int test_camera()
 {
@@ -73,8 +75,8 @@ int test_camera()
     // ------ reserve buffer space
     buff = pmsis_l2_malloc(BUFF_SIZE);
     if (buff == NULL){ return -1;}
-    buff_demosaick = pmsis_l2_malloc(BUFF_SIZE*3);
-    if (buff_demosaick == NULL){ return -1;}
+    // buff_demosaick = pmsis_l2_malloc(BUFF_SIZE*3);   // this buffer is used for color image
+    // if (buff_demosaick == NULL){ return -1;}
     printf("Initialized buffers\n");
 
     // ------ open flash
@@ -90,22 +92,12 @@ int test_camera()
     printf("Open Hyper flash\n");
 
 #if WRITE_DATASET
+    // ------ erase flash
     pi_flash_erase_chip(&flash);
     printf("Erase done\n");
 
     // ------ open uart
-    // ----- For ordinary Uart read
     pi_pad_set_function(PI_PAD_46_B7_SPIM0_SCK, PI_PAD_46_B7_SPIM0_SCK_FUNC0);
-    // static struct pi_device uart;
-    // struct pi_uart_conf uart_conf;
-    // pi_uart_conf_init(&uart_conf);
-    // pi_open_from_conf(&uart, &uart_conf);
-    // if (pi_uart_open(&uart))
-    // {
-    //     printf("UART open failed !\n");
-    //     pmsis_exit(-1);
-    // }
-    // printf("Open uart\n");
     // ----- For async Uart read
     struct pi_uart_conf uart_conf;
     pi_uart_conf_init(&uart_conf);
@@ -118,8 +110,7 @@ int test_camera()
         printf("Uart open failed !\n");
         pmsis_exit(-1);
     }
-    // [change]
-    // pi_uart_read_async(&uart, buff_uart_rcv, UART_RCV_SIZE, pi_task_callback(&taskUart, (void *) uart_rx_cb, NULL));
+    pi_uart_read_async(&uart, buff_uart_rcv, UART_RCV_SIZE, pi_task_callback(&taskUart, (void *) uart_rx_cb, NULL));
     printf("Start receiving !\n");
     
     // ------ open camera
@@ -168,53 +159,76 @@ int test_camera()
             {buff_packet_tmp[i]=buff_packet[i];}
 
         pi_pad_set_function(PI_PAD_46_B7_SPIM0_SCK, PI_PAD_46_B7_SPIM0_SCK_FUNC0);
-        // ----- For ordinary uart read
-        // pi_uart_read(&uart, buff_uart_rcv, UART_RCV_SIZE);
-        // buff_strip(buff_uart_rcv, buff_packet);
-        // decode_packet(buff_packet, &roll, &pitch, &pos_x, &pos_y, &pos_z, &pos_d);
 
         // printf("tick: %f, pos_z %f\n", cf_tick, pos_z);
 
-        // if(previousRoll != roll){
-            led_val ^= 1;
-            pi_gpio_pin_write(&gpio_device, 2, led_val);    // [LED blink]
-            // if(start_storing){
-            if(true){
-                // Crop from 244x324 to 224x320   [why do this step? slow down the speed]
-                int ps=0;
-                for(int i=0; i<244; i++){
-                    for(int j=0; j<324; j++){
-                        if ( i<234 && i>9 && j<322 && j>1){
-                            buff[ps] = buff[i*324+j];
-                            ps++;        			
-                        }
+        led_val ^= 1;
+        pi_gpio_pin_write(&gpio_device, 2, led_val);    // [LED blink]
+
+        printf("%d\n",start_storing);
+        if(start_storing)
+        {
+            // if(true){
+            // Crop from 244x324 to 224x320   [why do this step? slow down the speed]
+            int ps=0;
+            for(int i=0; i<244; i++){
+                for(int j=0; j<324; j++){
+                    if ( i<234 && i>9 && j<322 && j>1){
+                        buff[ps] = buff[i*324+j];
+                        ps++;        			
                     }
                 }
-                // Uncomment the following code for sampling the calibration images
-                // sprintf(image_name, "../../../images/img%05d.ppm", image_number);
-                // demosaicking(buff, buff_demosaick, 320, 224, 0);
-                // WriteImageToFile(image_name, 320, 224, sizeof(uint32_t), buff_demosaick, RGB888_IO);
-
-                // Saving real-time flight images and drone states to flash
-                pi_pad_set_function(PI_PAD_46_B7_SPIM0_SCK, PI_PAD_46_B7_HYPER_DQ6_FUNC3); // HW bug
-                pi_flash_program(&flash, flash_address, buff, (uint32_t) CROP_SIZE);
-                flash_address += CROP_SIZE;
-                pi_flash_program(&flash, flash_address, buff_packet_tmp, (uint32_t) PACKET_SIZE);
-                flash_address += PACKET_SIZE;
-                /* pi_flash_program(&flash, flash_address, buff_demosaick, (uint32_t) BUFF_SIZE3);
-                flash_address += BUFF_SIZE3; */
             }
+            // Uncomment the following code for sampling the calibration images
+            // sprintf(image_name, "../../../images/img%05d.ppm", image_number);
+            // demosaicking(buff, buff_demosaick, 320, 224, 0);
+            // WriteImageToFile(image_name, 320, 224, sizeof(uint32_t), buff_demosaick, RGB888_IO);
+
+            // Saving real-time flight images to flash
+            pi_pad_set_function(PI_PAD_46_B7_SPIM0_SCK, PI_PAD_46_B7_HYPER_DQ6_FUNC3); // HW bug
+            pi_flash_program(&flash, flash_address, buff, (uint32_t) CROP_SIZE);
+            flash_address += CROP_SIZE;
+            // saving the uart data sent from CF to flash 
+            // (uart data is updated in high-frequency, we assume here the uart data is updated, which is captured as the same time as the image data)
+            pi_flash_program(&flash, flash_address, buff_packet_tmp, (uint32_t) PACKET_SIZE);
+            flash_address += PACKET_SIZE;
+
+            /* pi_flash_program(&flash, flash_address, buff_demosaick, (uint32_t) BUFF_SIZE3);
+            flash_address += BUFF_SIZE3; */
+            image_number ++;
+            printf("image num: %05d\n", image_number);
+        }
+
+        // [interesting bug]: the following start_storing cannot overwirte the one on top  
+        // if (image_number>3)
+        // {
+        //     start_storing = false;
+        //     printf("we have 3 images\n");
+        //     printf("%d\n",start_storing);
         // }
-        // previousRoll = roll;      // [consider to change]
-        image_number ++;
-        // printf("image num: %05d\n", image_number);
+
 #else
-        for (uint32_t i=0; i<BUFF_SIZE; i++){buff[i] = 0;}
+        for (uint32_t i=0; i<BUFF_SIZE; i++){buff[i] = 0;}                            // can we skip this part?
         pi_flash_read(&flash, flash_address, buff, (uint32_t) CROP_SIZE);
         flash_address += CROP_SIZE;
 
-        if( ((buff[0]==0xFF) && (buff[1]==0xFF)) || (image_number > READSIZE) ){    // [why we need READSIZE?]
-        // if( (buff[0]==0xFF) && (buff[1]==0xFF) ){
+        // check if reach the end of img sequence
+        // if( ((buff[0]==0xFF) && (buff[1]==0xFF)) || (image_number > READSIZE) ){   [Found the problem]: need a better way to check the end of img
+        
+        // One walk around: check the entire cropped image. 
+        // If all pixels are white, indicated as the end of img sequence.
+        IMG_END = true;                        // flag 
+        for(uint32_t i=0; i<CROP_SIZE; i++){
+                if(buff[i] != 0xFF){           // if any pixel is not 0XFF, the image is valide
+                    IMG_END = false;
+                    printf("detect at buff[i] = %d\n",i);
+                    break;                     // break the for loop  
+                }
+            } 
+
+        if( IMG_END ||(image_number > READSIZE) ){
+            // reach the end of img
+            printf("Reach the end of images\n");
             //------ write attitude and 3D pos to a txt file
             struct pi_fs_conf conf;
             pi_fs_conf_init(&conf);
@@ -223,26 +237,22 @@ int test_camera()
             pi_open_from_conf(&fs, &conf);
             if (pi_fs_mount(&fs))
                 return -1;
-            // void *File = pi_fs_open(&fs, "../../../images/label.txt", PI_FS_FLAGS_WRITE);
+
+            void *File = pi_fs_open(&fs, "../../../images/label.txt", PI_FS_FLAGS_WRITE);
             char line_data[100];
             flash_address = CROP_SIZE;
-
+            
             for (int i=0; i<image_number; i++){
                 pi_flash_read(&flash, flash_address, buff_packet, (uint32_t) (PACKET_SIZE));
                 flash_address += (CROP_SIZE+PACKET_SIZE);
-                // decode_packet(buff_packet, &roll, &pitch, &pos_x, &pos_y, &pos_z, &pos_d);
-
-                if (false){
-                decode_packet(buff_packet, &cf_tick, &pos_z, &acc_z, &gyro_x, &gyro_y, &gyro_z);  // cf_tick, pos_z, acc_z, gyro_x, gyro_y, gyro_z;
-
-                sprintf(line_data, "img%05d.ppm %f %f %f %f %f %f\n", i,\
-                    cf_tick, pos_z, acc_z, gyro_x, gyro_y, gyro_z);                                     // 
+    
+                decode_packet(buff_packet, &cf_tick, &pos_z);  // cf_tick, pos_z;
+                sprintf(line_data, "img%05d.ppm %f %f \n", i, cf_tick, pos_z);                                    
                 int size_line_data = 0;
                 while(line_data[size_line_data]!='\n'){size_line_data++;}
-                // pi_fs_write(File, line_data, size_line_data+1);
-                }
+                pi_fs_write(File, line_data, size_line_data+1);
             }
-            // pi_fs_close(File);
+            pi_fs_close(File);
             pi_fs_unmount(&fs);
 
             pi_flash_close(&flash);
@@ -251,9 +261,11 @@ int test_camera()
         sprintf(image_name, "../../../images/img%05d.ppm", image_number);
         // save gray image //
         WriteImageToFile(image_name, 320, 224, sizeof(uint8_t), buff, GRAY_SCALE_IO);
+
         // save color image //
         //demosaicking(buff, buff_demosaick, 320, 224, 0);
         //WriteImageToFile(image_name, 320, 224, sizeof(uint32_t), buff_demosaick, RGB888_IO);
+
         pi_flash_read(&flash, flash_address, buff_packet, (uint32_t) PACKET_SIZE);
         flash_address += PACKET_SIZE;
         image_number ++;
@@ -273,7 +285,7 @@ static float buff_strip(uint8_t *src, uint8_t *tar)
 {
     for (int i = UART_RCV_SIZE-1; i>0; i--)
     {
-        if( (src[i]==0xfe) && (src[i-1]!=0xfe) && (src[i-25]==0xfe) )
+        if( (src[i]==0xfe) && (src[i-1]!=0xfe) && (src[i-(PACKET_SIZE+1)]==0xfe) )
         {
             for(int j=0; j<PACKET_SIZE; j++){
                 tar[j] = src[i-PACKET_SIZE+j];
@@ -283,7 +295,7 @@ static float buff_strip(uint8_t *src, uint8_t *tar)
 }
 
 
-static float decode_packet(uint8_t *src, float * r, float * p, float * x, float * y, float * z, float * d)
+static float decode_packet(uint8_t *src, float * tick, float * pos_z)
 {
     union {
         float a;
@@ -292,80 +304,23 @@ static float decode_packet(uint8_t *src, float * r, float * p, float * x, float 
     for(int j=0; j<4; j++){
         thing.bytes[j] = src[0+j];
     }
-    *r = thing.a;
+    *tick = thing.a;
     for(int j=0; j<4; j++){
         thing.bytes[j] = src[4+j];
     }
-    *p = thing.a;
-    for(int j=0; j<4; j++){
-        thing.bytes[j] = src[8+j];
-    }
-    *x = thing.a;
-    for(int j=0; j<4; j++){
-        thing.bytes[j] = src[12+j];
-    }
-    *y = thing.a;
-    for(int j=0; j<4; j++){
-        thing.bytes[j] = src[16+j];
-    }
-    *z = thing.a;
-    for(int j=0; j<4; j++){
-        thing.bytes[j] = src[20+j];
-    }
-    *d = thing.a;
+    *pos_z = thing.a;
 }
 
 
 void uart_rx_cb(void *arg)
 {
     buff_strip(buff_uart_rcv, buff_packet);
-    // decode_packet(buff_packet, &roll, &pitch, &pos_x, &pos_y, &pos_z, &pos_d);
-    decode_packet(buff_packet, &cf_tick, &pos_z, &acc_z, &gyro_x, &gyro_y, &gyro_z); 
+
+    decode_packet(buff_packet, &cf_tick, &pos_z); 
     if(pos_z > 0.3f){ start_storing = true;}
-    // printf("roll: %2.2f %2.2f\n", roll, pitch);
-    // printf("tick: %f, pos_z %f\n", cf_tick, pos_z);
+
+    // printf("tick: %f, pos_z: %f \n", cf_tick, pos_z);
+
     pi_uart_read_async(&uart, buff_uart_rcv, UART_RCV_SIZE, pi_task_callback(&taskUart, (void *) uart_rx_cb, NULL));
 }
 
-// // [original]
-// static float decode_packet(uint8_t *src, float * r, float * p, float * x, float * y, float * z, float * d)
-// {
-//     union {
-//         float a;
-//         unsigned char bytes[4];
-//     } thing;
-//     for(int j=0; j<4; j++){
-//         thing.bytes[j] = src[0+j];
-//     }
-//     *r = thing.a;
-//     for(int j=0; j<4; j++){
-//         thing.bytes[j] = src[4+j];
-//     }
-//     *p = thing.a;
-//     for(int j=0; j<4; j++){
-//         thing.bytes[j] = src[8+j];
-//     }
-//     *x = thing.a;
-//     for(int j=0; j<4; j++){
-//         thing.bytes[j] = src[12+j];
-//     }
-//     *y = thing.a;
-//     for(int j=0; j<4; j++){
-//         thing.bytes[j] = src[16+j];
-//     }
-//     *z = thing.a;
-//     for(int j=0; j<4; j++){
-//         thing.bytes[j] = src[20+j];
-//     }
-//     *d = thing.a;
-// }
-
-// // [original]
-// void uart_rx_cb(void *arg)
-// {
-//     buff_strip(buff_uart_rcv, buff_packet);
-//     decode_packet(buff_packet, &roll, &pitch, &pos_x, &pos_y, &pos_z, &pos_d);
-//     if(pos_z<-0.2f){ start_storing = true;}
-//     printf("roll: %2.2f %2.2f\n", roll, pitch);
-//     pi_uart_read_async(&uart, buff_uart_rcv, UART_RCV_SIZE, pi_task_callback(&taskUart, (void *) uart_rx_cb, NULL));
-// }
